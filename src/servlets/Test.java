26114747bf11ee8647fbd3f77814b6bed142c583
Victor <victor.rousselot@etud.univ-angers.fr>
2017-03-10 09:36:37 +0100

    test

---
+++ src/servlets/Test.java
@@ -0,0 +1,34 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.Coyote;
+
+public class Test extends HttpServlet {
+
+	public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+
+		/* Création et initialisation du message. */
+		String paramAuteur = request.getParameter( "auteur" );
+		String message = "Transmission de variables : OK ! " + paramAuteur;
+
+		/* Création du bean */
+		Coyote premierBean = new Coyote();
+		/* Initialisation de ses propriétés */
+		premierBean.setNom( "Coyote" );
+		premierBean.setPrenom( "Wile E." );
+
+		/* Stockage du message et du bean dans l'objet request */
+		request.setAttribute( "test", message );
+		request.setAttribute( "coyote", premierBean );
+
+		/* Transmission de la paire d'objets request/response à notre JSP */
+		this.getServletContext().getRequestDispatcher( "/WEB-INF/test.jsp" ).forward( request, response );
+	} 
+}
