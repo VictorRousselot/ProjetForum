2112832961b9d837bd65eb8c681fa92def7cdca0
P-Barone <pb8330@googlemail.com>
2017-03-17 09:51:54 +0100

    la racine redirige vers l'accueil (et le lien "Reigns - Forum" aussi)

--- test1/src/servlets/Accueil.java
+++ test1/src/servlets/Accueil.java
@@ -7,32 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Accueil
- */
 @WebServlet(description = "page d'accueil du forum", urlPatterns = { "/Accueil" })
 public class Accueil extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	public static final String VUE = "/forum.jsp";
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Accueil() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+		}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
